3d03d900819d7eb219271a391581cf278919db81
justcallmebeef <bethanymitch10@gmail.com>
2018-12-06 15:29:17 -0700

    added delete method and updated movie component to pull in movie data

--- src/App.js
+++ src/App.js
@@ -4,18 +4,47 @@ import Home from './components/Home';
 import Movies from './components/Movies';
 import './App.css';
 
+const movieAPI = "https://movie-db-project.herokuapp.com/"
+
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      movies: []
+      movies: [], 
     }
   }
+
+  async componentDidMount() {
+    this.loadMovies()
+  }
+
+  loadMovies = () =>  {
+    fetch(movieAPI)
+    .then(response => response.json()) 
+    .then(response => this.setState({movies: response}))
+  }
+
+  deleteMovie = (event) => {
+    fetch(`${movieAPI}${event.target.id}`, {
+      method: 'DELETE', 
+    })
+    .then(() => this.loadMovies())
+  
+  }
+
+  // getMovies = () => {
+  //   let movies = this.state.movies
+  //   let mapMovies = movies.map(movie => {
+  //     return movie
+  //   })
+  // }
+
   render() {
     return (
       <div className="App">
       <Route path='/' exact component={Home}/>
-      <Route path='/movies' component={Movies}/>
+      <Route path='/movies' render={() => (<Movies deleteMovie={this.deleteMovie} movies={this.state.movies} />
+      )}/>
       </div>
     );
   }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,11 +1,27 @@
 import React, { Fragment } from 'react'
 import Header from './Header'
 
-const Movies = () => {
+const Movies = (props) => {
+    let movieList = props.movies.map(movie => {
+        return (
+            <Fragment>
+                <div>
+                    <ul>
+                        <li>{movie.title}</li>
+                        <li>{movie.director}</li>
+                        <li>{movie.year}</li>
+                        <li>{movie.rating}</li>
+                    </ul>
+                    <button onClick={props.deleteMovie} id={movie.id}>Delete Movie</button>
+                    <button>Edit</button>
+                </div>
+            </Fragment>
+        )
+    })
     return (
         <Fragment>
             <Header />
-        <p>Movies</p>
+            { movieList }
         </Fragment>
     )
 }
