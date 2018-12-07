98d95145c7e92c525619369d6b7a28b5e4d7b635
justcallmebeef <bethanymitch10@gmail.com>
2018-12-06 17:09:14 -0700

    added bootstrap and descriptions above info

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,25 +5,40 @@ const Movies = (props) => {
     let movieList = props.movies.map(movie => {
         return (
             <Fragment>
-                <div>
-                    <ul>
-                        <li>{movie.title}</li>
-                        <li>{movie.director}</li>
-                        <li>{movie.year}</li>
-                        <li>{movie.rating}</li>
-                    </ul>
+                <ul className="list-group">
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                        <p>{movie.title}</p><p>{movie.director}</p><p>{movie.year}</p>
+                        <span className="fa fa-star">Rating: {movie.rating}</span>
+                    </li>
                     <button onClick={props.deleteMovie} id={movie.id}>Delete Movie</button>
                     <button>Edit</button>
-                </div>
+                </ul>
             </Fragment>
         )
     })
     return (
         <Fragment>
             <Header />
+            <ul className="list-group">
+                    <li id="descriptions" className="list-group-item d-flex justify-content-between align-items-center">
+                        <p>Title</p><p>Director</p><p>Year</p>
+                        <span className="fa fa-star">Rating</span>
+                    </li>
+                </ul>
             { movieList }
         </Fragment>
     )
 }
 
 export default Movies
+
+
+{/* <div className="movieContainer">
+<ul>
+    <li>{movie.title}</li>
+    <li>{movie.director}</li>
+    <li>{movie.year}</li>
+    <li>{movie.rating}</li>
+</ul>
+
+</div> */}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
